d71a530020af56f0b65eb4fdb1048867755ba70e
Aaron Rincon <rlaaron0305@gmail.com>
2022-06-25 21:37:30 -0500

    se comento el codigo

--- app.js
+++ app.js
@@ -1,156 +1,156 @@
+/* Importing the axios and ethers libraries. */
 const axios = require('axios')
-const {ethers} = require('ethers')
+const { ethers } = require('ethers')
 
-// const address='0x805B11d92Dcf960f14f5828E0c0F10772eE1E0D8' //azupreme
-// const address='0xED5AF388653567Af2F388E6224dC7C4b3241C544'//azuki
-// const address='0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB'// crypto punks
-// const address='0x395585cDCBD89d11c47EAd0567dC5b1eEEED471E'// test
-const apiKey='MSV9PH9SH128GIBNEHW7IGP2WU2TN26RSU'
-const geturl= (address,apiKey)=>{
-    const url=`https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${apiKey}`
+
+/**
+ * It takes two arguments, an address and an apiKey, and returns a url.
+ * param address - The address of the contract you want to get the ABI for.
+ * param apiKey - Your API key from Etherscan.io
+ * returns A function that takes two arguments, address and apiKey, and returns a string.
+ */
+const geturl = (address, apiKey) => {
+    const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${apiKey}`
     return url;
 }
 
-const infuraUrl='https://mainnet.infura.io/v3/0f9bcfd505d7446cb8d7a69bdef43497'
+/* The url for the infura api. */
+const infuraUrl = 'https://mainnet.infura.io/v3/0f9bcfd505d7446cb8d7a69bdef43497'
+
 /**
- * It takes the contract address and the ABI from Etherscan and uses it to get the contract name, total
- * supply, cost, max supply, max mint amount per tx, and revealed.
- * </code>
+ * It takes in a url and an address, and returns an object with the name, cost, total supply, max
+ * supply, max mint amount per tx, max mint amount per wallet, and whether or not the contract has a
+ * revealed function.* 
+ * returns An object with the following properties:
+ * name, cost, totalSupply, maxsupply, maxtx, maxWallet, reveled, left
  */
- const getAbi=async(url,address)=>{
+const getAbi = async (url, address) => {
     let info = new Object();
-     const res=await axios.get(url)
-     const abi=JSON.parse(res.data.result)
-     const address2 = address;
+    const res = await axios.get(url)
+    const abi = JSON.parse(res.data.result)
+    const address2 = address;
     //  console.log(abi)
-     const provider=new ethers.providers.JsonRpcProvider(infuraUrl)
-     const contract=new ethers.Contract(
-       address2,
-       abi,
-       provider
-     )
-   
+    const provider = new ethers.providers.JsonRpcProvider(infuraUrl)
+    const contract = new ethers.Contract(
+        address2,
+        abi,
+        provider
+    )
+
     /* Checking if the contract has a name. If it does, it will return the name. If it doesn't, it will
     return "name not found". */
-     if(contract.name){
-         const name=await contract.name();
-         // console.log("name: ",name); 
-         info.name= `${name}\n`;
-     }else{
-         // console.log("name not found");
-         info.name= `:grey_question:\n`;
-     }
- 
-     
-     /* Checking if the contract has a cost or price. If it does, it will return the cost or price. If
-     it doesn't, it will return "Free". */
-     if(contract.cost){
-         const cost=await contract.cost();
-         costeth = cost/1000000000000000000;
-         info.cost = `${costeth.toString()}\n`
-     }else if(contract.price){
-         const cost=await contract.price();
-         costeth = cost/1000000000000000000;
-         info.cost = `${costeth.toString()}\n`
-     }else if(contract.PRICE){
-        const cost=await contract.PRICE();
-        costeth = cost/1000000000000000000;
-        info.cost = `${costeth.toString()}\n`
-    }else{
-         info.cost = "Free\n";
-     }
+    if (contract.name) {
+        const name = await contract.name();
+        info.name = `${name}\n`;
+    } else {
+        info.name = `:grey_question:\n`;
+    }
 
+    /* Checking if the contract has a cost, price, or PRICE function. If it does, it will return the
+    cost, price, or PRICE. If it doesn't, it will return "Free". */
+    if (contract.cost) {
+        const cost = await contract.cost();
+        costeth = cost / 1000000000000000000;
+        info.cost = `${costeth.toString()}\n`
+    } else if (contract.price) {
+        const cost = await contract.price();
+        costeth = cost / 1000000000000000000;
+        info.cost = `${costeth.toString()}\n`
+    } else if (contract.PRICE) {
+        const cost = await contract.PRICE();
+        costeth = cost / 1000000000000000000;
+        info.cost = `${costeth.toString()}\n`
+    } else {
+        info.cost = "Free\n";
+    }
+
+    
+
+    /* Checking if the contract has a total supply. If it does, it will return the total supply. If it
+    doesn't, it will return "total supply not found". */
     let totalSupply;
-     /* Checking if the contract has a total supply. If it does, it will return the total supply. If it
-     doesn't, it will return "total supply not found". */
-     if(contract.totalSupply){
-        totalSupply=await contract.totalSupply()
-        // console.log("totalSupply = ",totalSupply.toString())
-        // info[3]= `totalSupply = ${totalSupply.toString()}\n`;
+    if (contract.totalSupply) {
+        totalSupply = await contract.totalSupply()
         info.totalSupply = `${totalSupply.toString()}\n`;
-    }else{
-        // console.log("totalSupply not found");
-        // info[3]= `totalSupply not found\n`;
+    } else {
         info.totalSupply = `:grey_question:\n`;
     }
 
-    /* Checking if the contract has a max supply. If it does, it will return the max supply. If it
-    doesn't, it will return "max supply not found". */
+
+    /* Checking if the contract has a maxSupply, TOKEN_LIMIT, or MAX_SUPPLY function. If it does, it
+    will return the maxSupply, TOKEN_LIMIT, or MAX_SUPPLY. If it doesn't, it will return
+    ":grey_question:". */
     let maxSupply;
-     if(contract.maxSupply){
+    if (contract.maxSupply) {
         maxSupply = await contract.maxSupply()
-        //  info[4] = `maxSupply = ${ maxSupply.toString()}\n`
-         info.maxsupply = `${ maxSupply.toString()}\n`
-     }else if(contract.TOKEN_LIMIT){  
-         maxSupply = await contract.TOKEN_LIMIT()
-        // info[4] = `maxSupply = ${ maxSupply.toString()}\n`
-        info.maxsupply = `${ maxSupply.toString()}\n`
-    }else if(contract.MAX_SUPPLY){
-         maxSupply = await contract.MAX_SUPPLY()
-        // info[4] = `maxSupply = ${ maxSupply.toString()}\n`
-        info.maxsupply = `${ maxSupply.toString()}\n`
-    }else{
-        //  info[4] = "max Supply: not found\n";
-         info.maxsupply = ":grey_question:\n";
-     }
- 
-     /* Checking if the contract has a max mint amount per tx. If it does, it will return the max mint
-     amount per tx. If it doesn't, it will return "max mint amount per tx not found". */
-     if(contract.maxMintAmountPertx){
-         const maxMintAmountPerTx = await contract.maxMintAmountPertx()
-        //  info[5] = `maxTx = ${maxMintAmountPerTx.toString()}\n`
-         info.maxtx = `${maxMintAmountPerTx.toString()}\n`
-     }else if(contract.MAX_JUDGE_MINT){
-        const  maxMintAmountPerTx = await contract.MAX_JUDGE_MINT()
-        // info[5] = `maxTx = ${ maxMintAmountPerTx.toString()}\n`
-        info.maxtx = `${ maxMintAmountPerTx.toString()}\n`
-    }else if(contract.maxMintSupply){
-        const  maxMintAmountPerTx = await contract.maxMintSupply()
-        // info[5] = `maxTx = ${ maxMintAmountPerTx.toString()}\n`
-        info.maxtx = `${ maxMintAmountPerTx.toString()}\n`
-    }else if(contract.maxMintAmount){
-        const maxMintAmountPerTx = await contract.maxMintAmount()
-        info.maxtx= `${ maxMintAmountPerTx.toString()}\n`
-    }else{
-        //  info[5] = "maxTx: not found\n";
-         info.maxtx = ":grey_question:\n";
+        info.maxsupply = `${maxSupply.toString()}\n`
+    } else if (contract.TOKEN_LIMIT) {
+        maxSupply = await contract.TOKEN_LIMIT()
+        info.maxsupply = `${maxSupply.toString()}\n`
+    } else if (contract.MAX_SUPPLY) {
+        maxSupply = await contract.MAX_SUPPLY()
+        info.maxsupply = `${maxSupply.toString()}\n`
+    } else{
+        info.maxsupply = ":grey_question:\n";
     }
 
-     if(contract.PUBLIC_MINT_LIMIT_PER_WALLET){
-         const maxMintWallet = await contract.PUBLIC_MINT_LIMIT_PER_WALLET()
-        //  info[6] = `maxWallet = ${maxMintWallet.toString()}\n`
-         info.maxWallet = `${maxMintWallet.toString()}\n`
-     }else if(contract.MAX_PER_ACCOUNT){
+
+    /* Checking if the contract has a maxMintAmountPertx, MAX_JUDGE_MINT, maxMintSupply, or
+    maxMintAmount function. If it does, it will return the maxMintAmountPertx, MAX_JUDGE_MINT,
+    maxMintSupply, or maxMintAmount. If it doesn't, it will return ":grey_question:". */
+    if (contract.maxMintAmountPertx) {
+        const maxMintAmountPerTx = await contract.maxMintAmountPertx()
+        info.maxtx = `${maxMintAmountPerTx.toString()}\n`
+    } else if (contract.MAX_JUDGE_MINT) {
+        const maxMintAmountPerTx = await contract.MAX_JUDGE_MINT()
+        info.maxtx = `${maxMintAmountPerTx.toString()}\n`
+    } else if (contract.maxMintSupply) {
+        const maxMintAmountPerTx = await contract.maxMintSupply()
+        info.maxtx = `${maxMintAmountPerTx.toString()}\n`
+    } else if (contract.maxMintAmount) {
+        const maxMintAmountPerTx = await contract.maxMintAmount()
+        info.maxtx = `${maxMintAmountPerTx.toString()}\n`
+    } else {
+        info.maxtx = ":grey_question:\n";
+    }
+
+    /* Checking if the contract has a maxMintAmountPertx, MAX_JUDGE_MINT, maxMintSupply, or
+        maxMintAmount function. If it does, it will return the maxMintAmountPertx, MAX_JUDGE_MINT,
+        maxMintSupply, or maxMintAmount. If it doesn't, it will return ":grey_question:". */
+    if (contract.PUBLIC_MINT_LIMIT_PER_WALLET) {
+        const maxMintWallet = await contract.PUBLIC_MINT_LIMIT_PER_WALLET()
+        info.maxWallet = `${maxMintWallet.toString()}\n`
+    } else if (contract.MAX_PER_ACCOUNT) {
         const maxSupply = await contract.MAX_PER_ACCOUNT()
-        // info[6] = `maxWallet = ${maxSupply.toString()}\n`
         info.maxWallet = `${maxSupply.toString()}\n`
-    }else if(contract.MAX_PER_WALLET){
+    } else if (contract.MAX_PER_WALLET) {
         const maxSupply = await contract.MAX_PER_WALLET()
-        info.maxWallet = `${ maxSupply.toString()}\n`
-    }else if(contract.limitPerAccount){
+        info.maxWallet = `${maxSupply.toString()}\n`
+    } else if (contract.limitPerAccount) {
         const maxSupply = await contract.limitPerAccount()
-        info.maxWallet = `${ maxSupply.toString()}\n`
-    }else{
-        //  info[6] = "maxWallet: not found\n";
-         info.maxWallet = ":grey_question:\n";
-     }
- 
-     /* Checking if the contract has a revealed function. If it does, it will return the revealed
-     function. If it doesn't, it will return "revealed not found". */
-     if(contract.revealed){
-         const revealed = await contract.revealed()
-        //  info[7] = `revealed: ${revealed.toString()}\n`
-         info.reveled = `${revealed.toString()}\n`
-     }else{
-        //  info[7] = "revealed: not found\n";
-         info.reveled = "Insta Reveal\n";
-     } 
-     let left =  maxSupply - totalSupply;
-    //  info[8] = `left: ${left.toString()}\n`;
-     info.left = `${left.toString()}\n`;
-     return info;
- 
- }
+        info.maxWallet = `${maxSupply.toString()}\n`
+    } else {
+        info.maxWallet = ":grey_question:\n";
+    }
+
+
+    /* Checking if the contract has a revealed function. If it does, it will return the value of the
+    revealed function. If it doesn't, it will return "Insta Reveal". */
+    if (contract.revealed) {
+        const revealed = await contract.revealed()
+
+        info.reveled = `${revealed.toString()}\n`
+    } else {
+        info.reveled = "Insta Reveal\n";
+    }
+
+    /* Subtracting the total supply from the max supply and then adding it to the info object. */
+    let left = maxSupply - totalSupply;
+    info.left = `${left.toString()}\n`;
+
+    return info;
+
+}
 // geturl()
 
 // getAbi(geturl(address,apiKey))
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* This is the code that connects the bot to the discord server. */
 const Discord = require("discord.js")
 const intents = new Discord.Intents(131071)
 const client = new Discord.Client({ intents })
@@ -6,29 +7,28 @@ require('dotenv').config();
 const { getAbi, geturl } = require("./app");
 const apiKey = 'MSV9PH9SH128GIBNEHW7IGP2WU2TN26RSU'
 
+/*Activate the bot and check the status*/
 client.once("ready", (bot) => {
     console.log(`Bot: ${bot.user.username}\nStatus: ${bot.presence.status}`);
 })
+
 client.on('messageCreate', async (msg) => {
-    const prefix = "$";
+    const prefix = "$"; //command for call the bot
     if (msg.author.bot) return;
-
-    // if (!msg.content.startsWith(prefix))
-    //     return msg.reply("Esto no es un comando");
-
+    /* This is the code that checks if the message starts with the prefix and then it splits the
+    message into an array and then it takes the first element of the array and converts it to
+    lowercase. */
     if (msg.content.startsWith(prefix)) {
-        const argumentos = msg.content.slice(prefix.length).split(/ +/);
-        const comando = argumentos.shift().toLowerCase();
-        const addres2 = argumentos.toString(16);
+        const arguments = msg.content.slice(prefix.length).split(/ +/);
+        const comand = arguments.shift().toLowerCase();
+        const addres2 = arguments.toString(16);
         const resultPrmoise = getAbi(geturl(addres2, apiKey), addres2);
         (async () => {
             const result = await Promise.resolve(resultPrmoise)
-            console.log(result);
-
-            if (comando == "c") {
-                const embed = new Discord.MessageEmbed()
+            if (comand == "c") {
+                /* Creating a message embed. */
+                const embed = new Discord.MessageEmbed()//
                     .setColor("BLUE")
-                    // .setAuthor(msg.author.username, msg.author.avatarURL())
                     .setTitle(`:pushpin: ${result.name}`)
                     .addFields(
                         { name: ":moneybag: Cost:", value: `${result.cost}`, inline: true },
@@ -40,15 +40,11 @@ client.on('messageCreate', async (msg) => {
                         { name: ":small_blue_diamond: Left", value: result.left, inline: true },
                     )
                     .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png")
-                    // .setThumbnail("https://w7.pngwing.com/pngs/268/1013/png-transparent-ethereum-eth-hd-logo-thumbnail.png")
-                    // .setImage("https://img.seadn.io/files/b40b563604decde9160b2b3cd7b018fc.jpg?fit=max&auto=format")
-                    // .setFooter("InfoBot")
-                    // .setTimestamp()
                 msg.reply({ embeds: [embed], components: [] })
-
             }
         })();
     }
 
 })
+/* discord bot token */
 client.login(process.env.DSTOKEN)
